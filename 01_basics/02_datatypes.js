a6c5658d68eefce2f46cbad8adba1bbf64e68999
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-02-27 15:06:16 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+// alert( 3 +3) // we are using node.js notbrowser
+
+
+console.log(3+3)  //code readability should be higher 
+
+console.log("geetanjali")
+
+
+let name ="Geetanjali"
+let age =23;
+let isLoggedIn = false 
+let state = null;
+
+//number => to power 53
+//bigInt 
+//String  => ""
+//boolean => true/false
+
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object 
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof age)
+
+
+
+
