fa88a58e0d1d792d14559b0fa4658203a4637a89
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-03-19 04:07:24 +0000

    commit

---
+++ 01_basics/Practice/Variable.js
@@ -0,0 +1,10 @@
+// Print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Function to check if a number is even or odd
+function isEvenOrOdd(num) {
+    return num % 2 === 0 ? "Even" : "Odd";
+}
+console.log(isEvenOrOdd(5)); // Odd
