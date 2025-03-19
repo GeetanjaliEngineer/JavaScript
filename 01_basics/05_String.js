38fdcd255c0e8c5b51836969afdcb0c95fffeeec
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-03-19 03:53:39 +0000

    commit

---
+++ 01_basics/05_String.js
@@ -0,0 +1,47 @@
+const name ="gitanjali"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+console.log('Hello My Name is ${name} and my repo count is${repoCount}');
+
+const gameName = new String ('gitanjali');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(0,4)
+console.log(anotherString);
+
+const anotherstringg = gameName.slice(-8, 4)
+console.log(anotherstringg);
+
+
+const newStringOne = " gitanjali"
+console.log( newStringOne);
+cosole.log(newStringOne.trim());
+
+
+const url = "....";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
