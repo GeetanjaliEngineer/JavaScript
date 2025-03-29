9c75bc6b73fd9f31c47d59077b37ed0f92d36082
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-03-29 07:13:45 +0000

    commit

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,29 @@
+//arrays
+
+
+const myArr =[ 0,1,2,3,4,5]
+const myHeros = ["Shaktiman", "naggraj"]
+console.log(myArr[0]);
+
+const myArray2 = new Array (1,2,3,4);
+
+ console.log(myArr[1]);
+//shallow copies - same reference 
+// //deep  copy - do not share same reference
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(-19));
+
+const newArr = myArr.join()
+console.log(myArr);
+
+console.log(newArr);
+
+
+
